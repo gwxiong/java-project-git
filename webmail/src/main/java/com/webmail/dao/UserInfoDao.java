438b397a4fdd95a4b026610e593331ecd0852db5
guowenxiong <3227762756@qq.com>
2017-01-20 16:21:47 +0800

    clear

--- webmail/src/main/java/com/webmail/dao/UserInfoDao.java
+++
@@ -1,7 +0,0 @@
-package com.webmail.dao;
-
-import com.webmail.domain.UserInfo;
-
-public interface UserInfoDao {
-	public void save(UserInfo userInfo);
-}
--- webmail/src/main/java/com/webmail/service/UserInfoService.java
+++
@@ -1,7 +0,0 @@
-package com.webmail.service;
-
-import com.webmail.domain.UserInfo;
-
-public interface UserInfoService {
-	public void add(UserInfo userInfo);
-}
